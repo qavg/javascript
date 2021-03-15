83ede8bb8a7f38f331b6fee0ba2bef7b512e98a9
qavg <vqa.swe.sp21@gmail.com>
2021-03-14 23:28:09 -0400

    :sparkles: Add array destructuring

--- destructuring.js
+++ destructuring.js
@@ -61,3 +61,32 @@ var { firstName, lastName, location } = getUser();
 console.log("🚀 ~ file: destructuring.js ~ line 61 ~ location", location);
 console.log("🚀 ~ file: destructuring.js ~ line 61 ~ lastName", lastName);
 console.log("🚀 ~ file: destructuring.js ~ line 61 ~ firstName", firstName);
+
+/* 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                                    Array Destructuring
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                
+*/
+// array notation
+var user = ["firstName", "lastName", "location"];
+var firstName = user[0];
+console.log("🚀 ~ file: destructuring.js ~ line 73 ~ firstName", firstName);
+var lastName = user[1];
+console.log("🚀 ~ file: destructuring.js ~ line 75 ~ lastName", lastName);
+var location = user[2];
+console.log("🚀 ~ file: destructuring.js ~ line 76 ~ location", location);
+
+// array destructing: same outcome
+var user = ["firstName", "lastName", "location"];
+var [firstName, lastName, location] = user;
+console.log("🚀 ~ file: destructuring.js ~ line 80 ~ location", location);
+console.log("🚀 ~ file: destructuring.js ~ line 80 ~ lastName", lastName);
+console.log("🚀 ~ file: destructuring.js ~ line 80 ~ firstName", firstName);
+
+var csv = "a, b, c, d, e";
+var [field1, field2, field3, field4, field5] = csv.split(",");
+console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field1", field1);
+console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field2", field2);
+console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field3", field3);
+console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field4", field4);
+console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field5", field5);
