6c30523137c9b923c93e5ceb440055eddad1ffce
qavg <vqa.swe.sp21@gmail.com>
2021-03-14 23:50:06 -0400

    :sparkles: Add destructuring function parameters

--- destructuring.js
+++ destructuring.js
@@ -90,3 +90,75 @@ console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field2", field2);
 console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field3", field3);
 console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field4", field4);
 console.log("🚀 ~ file: destructuring.js ~ line 88 ~ field5", field5);
+
+/* 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    Destructuring: Renaming poorly named object properties
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                
+*/
+
+var user = {
+  fn: "firstName",
+  ln: "lastName",
+  l: "location",
+};
+
+var { fn: firstName, ln: lastName, l: location } = user;
+console.log("🚀 ~ file: destructuring.js ~ line 107 ~ firstName", firstName);
+console.log("🚀 ~ file: destructuring.js ~ line 107 ~ lastName", lastName);
+console.log("🚀 ~ file: destructuring.js ~ line 107 ~ location", location);
+
+/* 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                    Destructuring: Function parameters
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~                
+*/
+// Avoid having to worry about parameters ordering:
+
+function fetchStuff(arg1, arg2, arg3, arg4, arg5) {
+  pass;
+}
+
+fetchStuff("arg1", 100, new Date("01/01/2021").getTime(), null, NaN);
+
+// vs passing an object to be destructured
+function fetchStuff({ arg1, arg2, arg3, arg4, arg5 }) {
+  pass;
+}
+
+fetchStuff({
+  arg1: null, // argument not caring about
+  arg2: null, // argument not caring about
+  arg3: new Date("01/01/2021").getTime(),
+  arg4: null,
+  arg5: NaN,
+});
+
+// Handling arguments we don't care about
+function fetchStuff({ arg1, arg2, arg3, arg4, arg5 }) {
+  pass;
+}
+
+fetchStuff({
+  arg3: new Date("01/01/2021").getTime(),
+  arg4: null,
+  arg5: NaN,
+});
+
+//   => set default values
+function fetchStuff({ arg1, arg2, arg3, arg4, arg5 }) {
+  arg1 = arg1 || "default value";
+  arg2 = arg2 || "default value";
+  arg3 = arg3 || "default value";
+  arg4 = arg4 || "default value";
+  arg5 = arg5 || "default value";
+}
+
+// with destructuring:
+function fetchStuff({
+  arg1 = "default value",
+  arg2 = "default value",
+  arg3 = "default value",
+  arg4 = "default value",
+  arg5 = "default value",
+}) {}
